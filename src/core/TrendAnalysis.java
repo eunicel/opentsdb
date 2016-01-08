cde88a5df8f4be66d207164aca9f2c1815e99e67
Eunice <elin1@directv.com>
2016-01-08 12:02:44 -0800

    switched to using asyncbase, created initial trends table, and builds successfully

--- src/core/TrendAnalysis.java
+++ src/core/TrendAnalysis.java
@@ -1,21 +1,59 @@
-import org.apache.hadoop.hbase.*;
+package net.opentsdb.core;
 
-public final class TrendAnalysis {
+import java.io.IOException;
+
+import net.opentsdb.utils.Config;
+
+import org.hbase.async.HBaseClient;
+import org.hbase.async.KeyValue;
+import org.hbase.async.PutRequest;
+
+import com.stumbleupon.async.Deferred;
+
+public class TrendAnalysis {
 	
-	private HBaseConfiguration conf;
-	private HBaseAdmin admin;
-	private HTableDescriptor table;
-	private HColumnDescriptor mean;
-	private HColumnDescriptor standardDeviation;
+	private static HBaseClient client;
+	private final Config config;
+	private static byte[] table;
+	private static final byte[] FAMILY = { 't' };
 	
-	public TrendAnalysis() {
-		conf = HBaseConfiguration.create();
-		admin = new HBaseAdmin(conf);
-		table = new HTableDescriptor("trends");
-		mean = new HColumnDescriptor(toBytes("mean"));
-		standardDeviation = new HColumnDescriptor(toBytes("standard deviation"));
-		table.add(mean);
-		table.add(standardDeviation);
+	/**
+	 * Creates a trends table in HBase that stores the mean
+	 * and standard deviation for each hour of each day of
+	 * the week.
+	 * @param args
+	 * @throws IOException
+	 */
+	public TrendAnalysis(final HBaseClient client, final Config config){
+		this.client = client;
+		this.config = config;
+		String tableName = "trends";
+		table = tableName.getBytes();
+		initializeRows();
 	}
-	test
+	
+	private static void initializeRows() {
+		KeyValue keyValue = null;
+		// Add rows for each hour of each day of the week
+		int[] weekdays = {1, 2, 3, 4, 5, 6, 7};
+		for(int day : weekdays) {
+			for(int i = 0; i < 24; i++) {
+				String rowName = day + "-" + i; // TODO: add metric
+				byte[] row = rowName.getBytes();
+				keyValue = new KeyValue(row, FAMILY, "mean".getBytes(), new byte[0]);
+				PutRequest point = new PutRequest(table, keyValue);
+				client.put(point);
+			}
+		}
+	}
+	
+	private void updateMetric(){
+		
+	}
+	
+	private static void getMetrics() {
+		
+	}
+	
+	
 }
