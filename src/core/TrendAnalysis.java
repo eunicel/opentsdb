eb682439b04d82c681a10298366034f384a50792
root <elin1@directv.com>
2016-01-12 15:13:33 -0800

    added getHour and getDay methods to get hour of day and day of week from timestamp, added metricsAndTags to keep track of all metrics and tags, and added allStats to keep track of all rows storing statistics in memory

--- src/core/TrendAnalysis.java
+++ src/core/TrendAnalysis.java
@@ -2,7 +2,10 @@ package net.opentsdb.core;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
@@ -26,7 +29,8 @@ public class TrendAnalysis {
 	private static byte[] table;
 	private static final byte[] FAMILY = { 't' };
 	static Logger log = LoggerFactory.getLogger(TrendAnalysis.class);
-	private Set<String> metrics;
+	private static Set<String> metricsAndTags;
+	private static HashMap<String, long[]> allStats;
 	
 	/**
 	 * Creates a trends table in HBase that stores the mean
@@ -39,7 +43,14 @@ public class TrendAnalysis {
 		log.info("in TrendAnalysis constructor");
 		this.client = client;
 		this.config = config;
-		metrics = new HashSet<String>();
+		
+		// a set of metrics-tags
+		metricsAndTags = new HashSet<String>();
+		
+		// mapping of metrics-tags-day-time to an
+		// array of count, mean, and standard deviation
+		allStats = new HashMap<String, long[]>(); 
+		
 		String tableName = "trends";
 		table = tableName.getBytes();
 	}
@@ -49,16 +60,26 @@ public class TrendAnalysis {
 	 * Creates a row for each hour of each day of the week for this metric.
 	 * @param metric
 	 */
-	private static void addMetric(String metric) {
-		log.info("start initializing rows for metric " + metric);
+	private static void addNewMetric(String newMetricAndTags, long timestamp, long value, Map<String, String> tags) {
+		log.info("start initializing rows for metric " + newMetricAndTags);
+		
+		metricsAndTags.add(newMetricAndTags);
+		
 		KeyValue mean = null;
 		KeyValue standardDev = null;
+		
 		// Add rows for each hour of each day of the week
 		int[] weekdays = {1, 2, 3, 4, 5, 6, 7};
 		for(int day : weekdays) {
 			for(int i = 0; i < 24; i++) {
 				log.info("row " + day + ", " + i);
-				String rowName = metric + "-" + day + "-" + i;
+				String rowName = newMetricAndTags + "-" + day + "-" + i;
+				
+				// Store in memory
+				long[] stats = {1, value, 0}; // count, mean, standard deviation
+				allStats.put(rowName, stats);
+				
+				// Store into HBase
 				byte[] row = rowName.getBytes();
 				mean = new KeyValue(row, FAMILY, "mean".getBytes(), new byte[0]);
 				standardDev = new KeyValue(row, FAMILY, "standard_deviation".getBytes(), new byte[0]);
@@ -87,21 +108,101 @@ public class TrendAnalysis {
 	         config);
 	  }
 	
-	public void addPoint(String metric, long timestamp, long value, Map<String, String> tags) {
+	/**
+	 * Adds a new point, updates the count, mean, and standard
+	 * deviation if it exists. Otherwise, create new rows for
+	 * the metric and initialize count, mean, and
+	 * standard deviation according to the newly added point.
+	 * @param metric
+	 * @param timestamp
+	 * @param value
+	 * @param tags
+	 */
+	public void addPoint(String metric,
+			long timestamp, long value, Map<String, String> tags) {
 		log.info("trendAnalysis adding point!!!!!!!!!!!!!! !!!!!!!!");
 		ArrayList<String> tagsList = new ArrayList<String>(tags.keySet());
 		Collections.sort(tagsList);
-		String metricAndTags = metric;
+		String newMetricAndTags = metric;
 		for(String tag : tagsList) {
-			metricAndTags = metricAndTags + "-" + tag;
+			newMetricAndTags = newMetricAndTags + "-" + tag;
 		}
-		if(metrics.contains(metricAndTags)) {
-			// update mean and standard deviation
+		if(metricsAndTags.contains(newMetricAndTags)) {
+			String rowName = newMetricAndTags + getDay(timestamp) + getHour(timestamp);
+			long[] stats = allStats.get(rowName);
+			// update stats in memory
+			allStats.put(rowName, updateStats(stats, value));
 		} else {
-			metrics.add(metricAndTags);
-			addMetric(metricAndTags);
+			addNewMetric(newMetricAndTags, timestamp, value, tags);
 		}
 	}
 	
+	/**
+	 * Updates the stats for a particular row with the new value.
+	 * Updates standard deviation according
+	 * https://en.wikipedia.org/wiki/Standard_deviation#Population-based_statistics
+	 * @param stats An array of count, mean, and standard deviation
+	 * @param value The new value to add
+	 * @return
+	 */
+	private long[] updateStats(long[] stats, long value) {
+		long count = stats[0];
+		long mean = stats[1];
+		long stdev = stats[2];
+		
+		stats[1] = ((mean * count) + value) / (count + 1); // update mean
+		stats[0] += 1; // update count
+		stats[2] = (long) Math.sqrt((count * Math.pow(stdev, 2) / (count + 1)
+				+ (count / Math.pow((count + 1), 2))
+				* Math.pow((mean - value), 2))); // update standard deviation
+		return stats;
+	}
+	
+	/**
+	 * Gets the day of the week given the epoch timestamp.
+	 * Monday = 1, Tuesday = 2, etc.
+	 * @param timestamp
+	 */
+	private int getDay(long timestamp) {
+		int day = 0;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date(timestamp * 1000));
+		int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
+		if(dayOfWeek == Calendar.MONDAY){
+			day = 1;
+		} else if (dayOfWeek == Calendar.TUESDAY) {
+			day = 2;
+		} else if (dayOfWeek == Calendar.WEDNESDAY) {
+			day = 3;
+		} else if (dayOfWeek == Calendar.THURSDAY) {
+			day = 4;
+		} else if (dayOfWeek == Calendar.FRIDAY) {
+			day = 5;
+		} else if (dayOfWeek == Calendar.SATURDAY) {
+			day = 6;
+		} else if (dayOfWeek == Calendar.SUNDAY) {
+			day = 7;
+		} else {
+			log.info("ERROR: Not a day of the week");
+		}
+		return day;
+	}
+	
+	/**
+	 * Gets the hour of the day given the epoch timestamp.
+	 * E.g., at 10:04:15.250 PM getHour will return 22.
+	 * @param timestamp
+	 * @return
+	 */
+	private int getHour(long timestamp) {
+		int hour;
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(new Date(timestamp * 1000));
+		return cal.get(Calendar.HOUR);
+	}
+	
+	private void addToHBase() {
+		
+	}
 	
 }
